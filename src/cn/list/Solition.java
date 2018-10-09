2dc0e371aadfa6e6e020a97aa2399b11959927f1
wangplcg <1173083839@qq.com>
2018-10-10 00:10:06 +0800

    '删除链表指定元素'

--- src/cn/list/Solition.java
+++ src/cn/list/Solition.java
@@ -2,7 +2,7 @@ package cn.list;
 
 /**
  * Created with IntelliJ IDEA.
- * Description:
+ * Description: 链表
  * User: Think
  * Date: 2018-10-08
  * Time: 23:14
@@ -16,6 +16,9 @@ public class Solition {
       ListNode(int x) { val = x; }
     }
 
+    /**
+     * 链表解决是否回文串1
+     */
     public static boolean isPalindrome(ListNode head) {
         if (head == null || head.next == null) {
             return true;
@@ -53,6 +56,9 @@ public class Solition {
         return true;
     }
 
+    /**
+     * 链表解决回文串（优化版 分步法）
+     */
     public static boolean isPalindrome2(ListNode head) {
         if (head == null || head.next == null) {
             return true;
@@ -83,6 +89,44 @@ public class Solition {
             p3 = p3.next;
         }
         return true;
+    }
+
+    /**
+     * 链表反转
+     */
+    public ListNode reverseList(ListNode head) {
+        ListNode p1 = head;
+        ListNode p2 = null;
+        ListNode p3 = null;
+
+        while (p1 != null) {
+            p2 = p1.next;
+            p1.next = p3;
+            p3 = p1;
+            p1 = p2;
+        }
+        return p3;
+    }
+
+    /**
+     * 移除链表指定元素
+     */
+    public ListNode removeElements(ListNode head, int val) {
+        if (head == null) {
+            return null;
+        }
+        ListNode node = new ListNode(Integer.MAX_VALUE);
+        node.next = head;
+
+        ListNode p1 = node;
+        while (p1 != null && p1.next != null) {
+            if (p1.next.val == val) {
+                p1.next = p1.next.next;
+            } else {
+                p1 = p1.next;
+            }
+        }
+        return node.next;
     }
 
     public static void main(String[] args) {
